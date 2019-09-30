bd78468a7fab9ad467584f81afc8c95efdff4b58
Bryan Abicht <ixentrik@gmail.com>
2019-09-29 23:57:21 -0700

    fixed error handlers

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-String.prototype.capFirstLetter = function() {
-  return this.charAt(0).toUpperCase() + this.substring(1);
-};
+String.prototype.capFirstLetter = function(){
+	return this.charAt(0).toUpperCase() + this.substring(1);
+}
 function getImages(num) {
-  fetch("https://dog.ceo/api/breeds/image/random/" + num)
+  fetch('https://dog.ceo/api/breeds/image/random/' + num)
     .then(res => res.json())
-    .then(dogData => $renderImages(dogData.message));
+    .then(dogData => $renderImages(dogData.message))
 }
 
 function breedSearch() {
-  fetch("https://dog.ceo/api/breeds/list/all")
+  fetch('https://dog.ceo/api/breeds/list/all')
     .then(res => res.json())
     .then(breedData => $renderBreeds(breedData.message));
 }
 
 function searchBreedImg(breed) {
-  let breedName = breed.toLowerCase();
-  fetch("https://dog.ceo/api/breed/" + breedName + "/images/random")
-    .then(res => res.json())
+let breedName = breed.toLowerCase();
+	fetch('https://dog.ceo/api/breed/' + breedName + '/images/random')
+		.then(res => res.json())
     // .then(breedImg => $renderSingle(breedImg.message))
     .then(breedImg => {
       console.log(breedImg);
-      if (breedImg.status == "error") {
-        console.log("Not applicable breed");
+      if (breedImg.status == "error"){
+        console.log("Not applicable breed")
         errorHandler("Not applicable breed");
-      } else if (breedImg.status == "success") {
-        $renderSingle(breedImg.message);
+      } else if (breedImg.status == "success"){
+        $renderSingle(breedImg.message)
       }
-    });
+    })
 }
 function $renderImages(data) {
   data.forEach(img => {
@@ -36,48 +36,50 @@ function $renderImages(data) {
   });
 }
 function $renderSingle(data) {
-  $(".dog-results").append(`
+ 	$('.dog-results').append(`
 		<img class="dog-result single" src="${data}" alt="dog">
 	`);
 }
-function $renderBreeds(data) {
-  let breedArr = [];
-  for (let breed in data) {
-    breedArr.push(breed.capFirstLetter());
-  }
-  breedArr.sort().forEach(breed => {
-    $(".breed_list").append(`
+function $renderBreeds(data){
+	let breedArr = [];
+	for(let breed in data){
+		breedArr.push(breed.capFirstLetter());
+	}
+	breedArr.sort().forEach(breed => {
+		$('.breed_list').append(`
 			<option value="${breed.toLowerCase()}">${breed}</option>
-		`);
-  });
+		`)		
+	})  
 }
 function theListener() {
   $("form").submit(e => {
-    e.preventDefault();
-    $(".dog-results").empty();
-    $(".error_handle").hide();
-  });
+  e.preventDefault();
+	$('.dog-results').empty();
+  $('.error_handle').hide();
+});
   $("#dog_lottery").submit(e => {
     let dogCount = $(".dog_total").val();
-    getImages(dogCount);
+   	if(dogCount > 50){
+			errorHandler("You cannot select more than 50 images. Please select a smaller number.");
+		} else {
+			getImages(dogCount);
+		}
   });
   $("#breed_searcher").submit(e => {
-    let breedName = $(".breed_names")
-      .val()
-      .toLowerCase();
+    let breedName = $(".breed_names").val().toLowerCase();
     searchBreedImg(breedName);
+
   });
   $("#breed_browser").submit(e => {
     let breedName = $(".breed_list").val();
     searchBreedImg(breedName);
+
   });
 }
-function errorHandler(errorMsg) {
-  $(".error_handle")
-    .text(errorMsg)
-    .show();
+function errorHandler(errorMsg){
+$('.error_handle').text(errorMsg).show();
 }
-$(function() {
-  breedSearch();
-  theListener();
-});
+$(function(){
+	breedSearch();
+	theListener();
+})
